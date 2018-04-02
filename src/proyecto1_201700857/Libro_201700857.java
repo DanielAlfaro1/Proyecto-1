4d8ecca1ac903a41f3ce1a01b063e8b1647a0e0e
Bminas <Bminas@Dalfaro117>
2018-04-02 17:25:24 -0600

    Esta temblando correeee Clase Libro

---
+++ src/proyecto1_201700857/Libro_201700857.java
@@ -0,0 +1,31 @@
+
+package proyecto1_201700857;
+
+
+public class Libro_201700857 {
+    public String Autor;
+    public int Año;
+    public int ISBN;
+    public String Titulo;
+    public int Edicion;
+    public String[] Claves = new String[3];
+    public String Descripcion;
+    public String[] Temas = new String[3];
+    public int Copias;
+    
+    public Libro_201700857 (String a, int y, int i, String t, int e,String c1,String c2,String c3,String d,String t1, String t2, String t3,int c){
+        this.Autor=a;
+        this.Año=y;
+        this.ISBN=i;
+        this.Titulo=t;
+        this.Edicion=e;
+        this.Claves[0]=c1;
+        this.Claves[1]=c2;
+        this.Claves[2]=c3;
+        this.Descripcion=d;
+        this.Temas[0]=t1;
+        this.Temas[1]=t2;
+        this.Temas[2]=t3;
+        this.Copias=c;
+    }
+}
